0dea65d26fc9043f2e06ca83412e429934fe24f9
JonOn <jon@buildschool.io>
2018-11-08 20:24:35 -0800

    Initial

---
+++ LookingBus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LookingBus
+//
+//  Created by Jonathan on 11/8/18.
+//  Copyright © 2018 Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
